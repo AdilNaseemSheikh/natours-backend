f86f454eb39584c7ef2d4bfdb325bb4863047500
AdilNaseemSheikh <adilnaseemsheikh@gmail.com>
2024-04-15 16:34:18 +0500

    error handling class added

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+
+const globalErrorHandler = require('./controllers/errorController.js');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -38,5 +42,28 @@ app.use((req, res, next) => {
 // iii) this process is called mounting a router on a specific route
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// if code reaches here, it did not enter any of above routes
+
+// all means every request type(get, post, patch, delete, put) & * means for all routes
+app.all('*', (req, res, next) => {
+  // i)
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server.`,
+  // });
+  // ii)
+  // const err = new Error(`Can't find ${req.originalUrl} on this server.`);
+  // err.statusCode = 404;
+  // err.status = 'fail';
+
+  // if we pass anything to the next, it will automatically be considered as error
+  // and all the middlewares in the stack will be skiped & Error handling
+  // middleware will be called with error passed to next.
+  // iii)
+  next(new AppError(`Can't find ${req.originalUrl} on this server.`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
