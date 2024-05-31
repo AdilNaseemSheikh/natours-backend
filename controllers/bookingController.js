8d2e813aca94ac983ecd0719746dc371586c5e2c
AdilNaseemSheikh <adilnaseemsheikh@gmail.com>
2024-05-31 22:40:22 +0500

    customer email corrected

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -71,10 +71,11 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = async (session) => {
-  console.log('creating booking with session--> ', session);
   const tour = session?.client_reference_id;
-  const user = (await User.find({ email: session?.customer_email }))?.id;
+  const user = (await User.find({ email: session?.customer_details.email }))
+    ?.id;
   const price = session.amount_total / 100;
+  console.log('booking data--> ', tour, user, price);
   await Booking.create({
     tour,
     user,
